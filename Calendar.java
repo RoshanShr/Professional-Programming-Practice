f7af99c37e5330fdf8c937b2d757fcc00dcf37f7
GrgArjun <gureung@gmail.com>
2019-08-20 15:41:45 +1000

    Changed the instance name CaLeNdAr to calender

--- Calendar.java
+++ Calendar.java
@@ -4,11 +4,11 @@ import java.util.concurrent.TimeUnit;
 public class Calendar {
 	
 	private static Calendar self; // local variable Self changed to self by Arjun @3:17PM
-	private static java.util.Calendar calendar; //Changed the instance name
+	private static java.util.Calendar calendar; //Changed the instance name from CaLeNdAr to calender
 	
 	
 	private Calendar() {
-		calendar = java.util.Calendar.getInstance(); //Changed the instance name
+		calendar = java.util.Calendar.getInstance(); //Changed the instance name from CaLeNdAr to calender
 	}
 	
 	public static Calendar INSTANCE() {
@@ -19,16 +19,16 @@ public class Calendar {
 	}
 	
 	public void incrementDate(int days) {
-		calendar.add(java.util.Calendar.Date, days); //Changed the instance name Calender and the class DATE to Date	
+		calendar.add(java.util.Calendar.Date, days); //Changed the instance name from CaLeNdAr to calender and the class DATE to Date	
 	}
 	
 	public synchronized void setDate(Date date) { //Changing the method name to meaningful verb phrase
 		try {
 			calendar.setTime(date); //Changed the instance name
-	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  //Changed the instance name
-	        calendar.set(java.util.Calendar.MINUTE, 0);  //Changed the instance name
-	        calendar.set(java.util.Calendar.SECOND, 0);  //Changed the instance name
-	        calendar.set(java.util.Calendar.MILLISECOND, 0); //Changed the instance name
+	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  //Changed the instance name from CaLeNdAr to calender
+	        calendar.set(java.util.Calendar.MINUTE, 0);  ////Changed the instance name from CaLeNdAr to calender
+	        calendar.set(java.util.Calendar.SECOND, 0);  ////Changed the instance name from CaLeNdAr to calender
+	        calendar.set(java.util.Calendar.MILLISECOND, 0); ////Changed the instance name from CaLeNdAr to calender
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
@@ -36,11 +36,11 @@ public class Calendar {
 	}
 	public synchronized Date date() { //Changing the method name to meaningful verb phrase
 		try {
-	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  //Changed the instance name
-	        calendar.set(java.util.Calendar.MINUTE, 0);  //Changed the instance name
-	        calendar.set(java.util.Calendar.SECOND, 0);  //Changed the instance name
-	        calendar.set(java.util.Calendar.MILLISECOND, 0); //Changed the instance name
-			return calendar.getTime(); //Changed the instance name
+	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  ////Changed the instance name from CaLeNdAr to calender
+	        calendar.set(java.util.Calendar.MINUTE, 0);  //Changed the instance name from CaLeNdAr to calender
+	        calendar.set(java.util.Calendar.SECOND, 0);  //Changed the instance name from CaLeNdAr to calender
+	        calendar.set(java.util.Calendar.MILLISECOND, 0); //Changed the instance name from CaLeNdAr to calender
+			return calendar.getTime(); ///Changed the instance name from CaLeNdAr to Calender
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
@@ -49,9 +49,9 @@ public class Calendar {
 
 	public synchronized Date dueDate(int loanPeriod) { //Changing the method name to meaningful verb phrase
 		Date NoW = Date();
-		 calendar.add(java.util.Calendar.DATE, loanPeriod); //Changed the instance name
-		Date DuEdAtE =  calendar.getTime(); //Changed the instance name
-		 calendar.setTime(NoW); //Changed the instance name
+		 calendar.add(java.util.Calendar.DATE, loanPeriod); //Changed the instance name from CaLeNdAr to calender
+		Date DuEdAtE =  calendar.getTime(); //Changed the instance name from CaLeNdAr to calender
+		 calendar.setTime(NoW); //Changed the instance name from CaLeNdAr to calender
 		return DuEdAtE;
 	}
 	
